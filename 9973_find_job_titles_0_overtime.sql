d569d65192cbcefbd82b7168d7c98a18030f870d
Terence Patrick <terence@terencepatrick.com>
2023-12-17 21:53:09 -0800

    commit

---
+++ 9973_find_job_titles_0_overtime.sql
@@ -0,0 +1,9 @@
+/*
+Find job titles that had 0 hours of overtime.
+Output unique job title names.
+Table: sf_public_salaries
+*/
+
+select distinct jobtitle
+from sf_public_salaries
+where overtimepay = 0;
